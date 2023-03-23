cad67eac029d89c6fa94557305bd8b6a534ff95d
Emmali Montgomery <emmalit@Emmas-Air.local>
2023-03-22 22:11:21 -0400

    added sponsors

--- src/components/home/Highlights.vue
+++ src/components/home/Highlights.vue
@@ -1,24 +1,63 @@
 <script setup>
 import HighlightItem from "./HighlightItem.vue";
+import SponsorItem from "./SponsorItem.vue";
+import GradientContainer from "../GradientContainer.vue";
 </script>
 
 <template>
   <div class="gradientContainer"></div>
   <div class="highlightsSection">
-    <h1>Are you ready to support learning and networking in your community?</h1>
-    <p>There are CPOSC sponsorship opportunities at all levels! <a href="mailto:sponsor@localareanetworks.org?subject=Sponsorship Lead: I'd like more info!" target="_blank">Contact us</a> for a prospectus and more information on how you can help this event benefit everyone from high school and college students to professionals with decades of experience.</p>
-    <div class="highlightsWrapper">
-      <HighlightItem v-for="link of quickLinks"
+    <!-- <h1>Are you ready to support learning and networking in your community?</h1> -->
+    <h1>CPOSC Sponsors, 2023</h1>
+    <!-- <p>There are CPOSC sponsorship opportunities at all levels! <a href="mailto:sponsor@localareanetworks.org?subject=Sponsorship Lead: I'd like more info!" target="_blank">Contact us</a> for a prospectus and more information on how you can help this event benefit everyone from high school and college students to professionals with decades of experience.</p> -->
+    <p>Thank you to our 2023 Sponsors for their support in community learning and networking! <a href="mailto:sponsor@localareanetworks.org?subject=Sponsorship Lead: I'd like more info for 2024!" target="_blank">Contact us</a> for information on how you can be involved next year.</p>
+    <!-- <div class="highlightsWrapper"> -->
+      <!-- <HighlightItem v-for="link of quickLinks"
         :title=link.title
         :content=link.content
         :icon=link.icon
-      />
+      /> -->
+    <!-- </div> -->
+    <div class="sponsorsWrapper">
+      <div class="sponsorSection" style="flex-direction: column">
+        <SponsorItem v-for="sponsor of sponsors.slice(0, 2)"
+          :name=sponsor.name
+          :tier=sponsor.tier
+        />
+      </div>
+      <div class="sponsorSection">
+        <SponsorItem v-for="sponsor of sponsors.slice(2, 4)"
+          :name=sponsor.name
+          :tier=sponsor.tier
+        />
+      </div>
+      <div class="sponsorSection">
+        <SponsorItem v-for="sponsor of sponsors.slice(4, 7)"
+          :name=sponsor.name
+          :tier=sponsor.tier
+        />
+      </div>
+      <div class="sponsorSection">
+        <SponsorItem v-for="sponsor of sponsors.slice(7, 12)"
+          :name=sponsor.name
+          :tier=sponsor.tier
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+
 export default {
+  components: {
+    Carousel,
+    Slide,
+    Pagination,
+    Navigation,
+  },
   data() {
     return {
       quickLinks: [
@@ -42,6 +81,20 @@ export default {
           content: "Develop your local community of technologists.",
           icon: "fa-solid fa-handshake-angle"
         }
+      ],
+      sponsors: [
+        { name: "Seisan", tier: "one" },
+        { name: "WebstaurantStore", tier: "one" },
+        { name: "Elastic", tier: "two" },
+        { name: "Listrak", tier: "two" },
+        { name: "Cargas", tier: "three" },
+        { name: "Mind Development & Design", tier: "three" },
+        { name: "Industrial Resolution", tier: "three" },
+        { name: "Boyd Technical Solutions", tier: "four" },
+        { name: "JFC Global", tier: "four" },
+        { name: "Crunchy Data", tier: "four" },
+        { name: "HUG", tier: "four" },
+        { name: "Quub", tier: "four" },
       ]
     }
   }
@@ -92,6 +145,39 @@ h1 {
 
     @media screen and (max-width: 850px) {
       flex-direction: column;
+    }
+  }
+
+  .sponsorsWrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: rgba(117, 117, 117, 0.5);
+    box-shadow: 0 0 30px 20px rgba(117, 117, 117, 0.5);
+    border-radius: 10px;
+
+    @media screen and (max-width: 663px) {
+      width: 100%;
+      padding: 2rem 0;
+      box-shadow: 0 0 50px 50px rgba(117, 117, 117, 0.5);
+      margin-top: 2rem;
+    }
+
+    .sponsorSection {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      padding: 1rem 3rem;
+      margin-bottom: 1rem;
+      width: 100%;
+      max-width: 800px;
+
+      @media screen and (max-width: 663px) {
+        max-width: 100%;
+        flex-direction: column;
+        padding: 0;
+      } 
     }
   }
 }
---
+++ src/components/home/SponsorItem.vue
@@ -0,0 +1,162 @@
+<template>
+  <div class="sponsorItem" data-aos="zoom-in" data-aos-duration="5000" data-aos-easing="ease-in-out">
+    <div v-bind:class="tier">
+      <img
+        v-if="name === 'Crunchy Data'"
+        :alt="name"
+        src="@/assets/sponsors/crunchy.png"
+      />
+      <img
+        v-if="name === 'Boyd Technical Solutions'"
+        :alt="name"
+        src="@/assets/sponsors/boyd.png"
+      />
+      <img
+        v-if="name === 'Cargas'"
+        :alt="name"
+        src="@/assets/sponsors/cargas.png"
+      />
+      <img
+        v-if="name === 'HUG'"
+        :alt="name"
+        src="@/assets/sponsors/hug-logo.png"
+      />
+      <img
+        v-if="name === 'Industrial Resolution'"
+        :alt="name"
+        src="@/assets/sponsors/indyres.png"
+      />
+      <img
+        v-if="name === 'JFC Global'"
+        :alt="name"
+        src="@/assets/sponsors/jfc.png"
+      />
+      <img
+        v-if="name === 'Listrak'"
+        :alt="name"
+        src="@/assets/sponsors/Listrak-Large.png"
+      />
+      <img
+        v-if="name === 'Quub'"
+        :alt="name"
+        src="@/assets/sponsors/quublogo.svg"
+      />
+      <img
+        v-if="name === 'Mind Development & Design'"
+        :alt="name"
+        src="@/assets/sponsors/mind.png"
+      />
+      <img
+        v-if="name === 'Seisan'"
+        :alt="name"
+        src="@/assets/sponsors/seisan.png"
+      />
+      <img
+        v-if="name === 'WebstaurantStore'"
+        :alt="name"
+        class="web"
+        src="@/assets/sponsors/webstaurant-logo-w-icon.svg"
+      />
+      <img
+        v-if="name === 'Elastic'"
+        :alt="name"
+        src="@/assets/sponsors/elastic-logo.svg"
+      />
+      <!-- <h4>{{ name }}</h4> -->
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    name: String,
+    tier: String,
+    key: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+h4 {
+  text-transform: uppercase;
+  color: white;
+  font-weight: 900;
+  letter-spacing: 2px;
+  font-size: 12px;
+  margin-bottom: 5px;
+  text-align: center;
+}
+
+p {
+  font-size: 12px;
+  color: #b1b3b1;
+  margin-top: 0;
+  max-width: 150px;
+}
+
+i {
+  color: #2b3480;
+  font-weight: 900;
+  font-size: 50px;
+  margin-right: 1rem;
+  margin-bottom: 15px;
+}
+
+.sponsorItem {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0rem 0.5rem;
+
+  @media screen and (max-width: 850px) {
+    margin-bottom: 2rem;
+  }
+
+  @media screen and (max-width: 433px) {
+    margin-bottom: 0.5rem !important;
+  }
+
+  img {
+    object-fit: contain;
+  }
+
+  .one > img {
+    // transform: scale(1.75)
+    width: 275px;
+
+    &.web {
+      width: 400px !important;
+
+      @media screen and (max-width: 433px) {
+        width: 265px !important;
+      }
+    }
+
+    @media screen and (max-width: 433px) {
+      width: 190px;
+    }
+  }
+
+  .two > img {
+    width: 190px;
+
+    @media screen and (max-width: 433px) {
+      width: 140px;
+    }
+  }
+
+  .three > img {
+    width: 140px;
+
+    @media screen and (max-width: 433px) {
+      width: 75px;
+    }
+  }
+
+  .four > img {
+    width: 75px;
+  }
+}
+</style>
